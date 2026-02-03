b8e74cdc8558bb4733b365d6539870c3e707aab2
Zerophyr <nekrophil@protonmail.com>
2026-02-03 06:08:25 +0100

    test: add release version bump helper

---
+++ tests/release-version.test.ts
@@ -0,0 +1,15 @@
+const { bumpVersion } = require("../scripts/release-utils.js");
+
+describe("bumpVersion", () => {
+  test("patch bump increments patch", () => {
+    expect(bumpVersion("1.1.1", "patch")).toBe("1.1.2");
+  });
+
+  test("minor bump increments minor and resets patch", () => {
+    expect(bumpVersion("1.1.1", "minor")).toBe("1.2.0");
+  });
+
+  test("major bump increments major and resets minor/patch", () => {
+    expect(bumpVersion("1.1.1", "major")).toBe("2.0.0");
+  });
+});
